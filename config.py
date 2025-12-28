0d9db313baabc65579f17486d29c964ae100e903
Arlan200 <belfortd805@gmail.com>
2025-12-28 18:31:36 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+﻿BOT_TOKEN = "8570032015:AAEllEYO3Bv6jsMCaHIV2sv77r0S48F_-YY"  # токен ботика
+CHANNEL_URL = "https://t.me/+yXkdMVZ59F0xYTFi"  # ссылка канала проверка подписки
+CHANNEL_ID = "-1002615110946"  # айди канала для проверки подписки
+VERIF_CHANNEL_ID = "-1003574648138"  # айди чата для верификации(обсуждение канала с постбеками)
+SUPP = "https://t.me/dealisnomore"
+ADMIN_ID = 1419567048 # Ваш айди без скобочек
+PROMO = "PRIME220" # промокод
